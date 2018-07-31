41be754267fbb982511ee9afd9e113a888b86ae9
Vineet Sinha <vineet.sinha@gmail.com>
2018-07-31 10:10:54 -0400

    adding violet to the route did not feel like it was necessary

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var violetSrvr = require('violet/lib/violetSrvr')('/violet');
+var violetSrvr = require('violet/lib/violetSrvr')();
 violetSrvr.listAppsAt('/');
 var srvrInstance = violetSrvr.createAndListen(process.env.PORT || 8080);
 
